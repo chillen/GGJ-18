8492dec778e275d03bfa0a2fe32c01a4d4186979
ChristopherBlackman <Mr.Christopher.Blackman@gmail.com>
2018-01-27 15:00:18 -0500

    1 News Paper

--- Final/Scripts/NewsPaperScript.gd
+++ Final/Scripts/NewsPaperScript.gd
@@ -1,19 +1,25 @@
 extends Control
 
 
-export(Color, RGB) var col = Color(255,255,255) # Color is RGB
-export(int) var page = 0
-export(int) var num_pages = 0
+
+export(int) var page = 1
+export(int) var num_pages = 1
 export(String) var  header = "Place Text"
-export(String) var  body = "Lorum Lisp"
+export(String) var  body = "Lorum Lisp "
+export(Image) var image
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	$News.modulate = col
 	$Header.text = header
 	$Page.text = "%s/%s" % [page,num_pages]
 	$Body.text = body
-	$Background.rect_size = $Body.rect_size
+	set_process(true)
+	
 
-	pass
+func _process(delta):
+	$BackgroundBody.rect_position = $Body.rect_position
+	$BackgroundBody.rect_size = $Body.rect_size
+
+
+    
