0a196f6144801ad848e5edd4f856b20744b5ffd2
Christopher Blackman <ChristopherBlackman@users.noreply.github.com>
2018-01-28 08:26:34 -0500

    newspaper

--- Final/Scripts/NewsPaperUI.gd
+++ Final/Scripts/NewsPaperUI.gd
@@ -10,6 +10,8 @@ var paper_changed = false
 var texts =  []
 var object = []
 
+var child = null
+
 var papers = [
 preload('res://Scenes/NewsPaperType1.tscn'), 
 preload('res://Scenes/NewsPaperType2.tscn'), 
@@ -17,26 +19,35 @@ preload('res://Scenes/NewsPaperType3.tscn'),
 preload('res://Scenes/NewsPaperType4.tscn')]
 
 
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	
+	
+	texts = MissionData.get_tonights_papers()
 	renew_papers()
 	set_process(true)
+	
 
 func generator():
 	return papers[floor((len(papers)*randf()))]
 	
 
 func renew_papers():
+	print("call")
+	if not child == null:
+		child.queue_free()
 	
 	var objectClass = generator()
 	object = objectClass.instance()
 	object.rect_position = self.rect_position
-	#print($ColorRect.rect_position)
-	object.header = "THE EMPIRE STRIKES BACK"
-	object.body   = "PUG"
+	
+	object.header = texts[active_paper].header
+	object.body   = texts[active_paper].text
+	
 	self.add_child(object)
+	child = object
 	
 
 
@@ -45,11 +56,18 @@ func renew_papers():
 func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
+	
 	if Input.is_action_just_pressed('ui_right'):
+		print("right")
+		active_paper += 1
+		renew_papers()
 		pass
 		#next_paper()
 	if Input.is_action_just_pressed('ui_left'):
+		print('left')
 		pass
 		#prev_paper()
+		
+		
 	
 	
