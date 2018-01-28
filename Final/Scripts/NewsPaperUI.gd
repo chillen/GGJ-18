49dbce7ee8421a748b8cb498bcd1f632ed439e93
Christopher Blackman <ChristopherBlackman@users.noreply.github.com>
2018-01-28 07:28:37 -0500

    loaded a newspaper

--- Final/Scripts/NewsPaperUI.gd
+++ Final/Scripts/NewsPaperUI.gd
@@ -6,70 +6,50 @@ extends Control
 
 var active_paper = 0
 var paper_changed = false
-var texts = ['']
+
+var texts =  []
+var object = []
+
+var papers = [
+preload('res://Scenes/NewsPaperType1.tscn'), 
+preload('res://Scenes/NewsPaperType2.tscn'), 
+preload('res://Scenes/NewsPaperType3.tscn'),
+preload('res://Scenes/NewsPaperType4.tscn')]
+
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	
 	renew_papers()
-	set_text(get_text())
 	set_process(true)
 
-func renew_papers():
-	NewsRelatedData.choose_newspaper()
-	texts = NewsRelatedData.get_list_newspaper()
+func generator():
+	return papers[floor((len(papers)*randf()))]
+	
 
-func set_text(text):
-	$Label.text = texts[active_paper]
-	pass
+func renew_papers():
 	
-func set_arrows():
-	if active_paper == 0:
-		$LeftArrow.visible = false
-	else:
-		$LeftArrow.visible = true
+	var objectClass = generator()
+	object = objectClass.instance()
+	object.rect_position = self.rect_position
+	#print($ColorRect.rect_position)
+	object.header = "THE EMPIRE STRIKES BACK"
+	object.body   = "PUG"
+	self.add_child(object)
 	
-func get_text():
-	return texts[active_paper]
-	
-func set_page():
-	$Page.text = '%s/%s' % [active_paper+1,len(texts)]
-	
-func next_paper():
-	active_paper += 1
-	paper_changed = true
-	pass
-	
-func prev_paper():
-	#active_paper = (active_paper - 1) % len(texts)
-	active_paper = max(0,(active_paper - 1))
-	paper_changed = true
-	pass
+
+
+
 
 func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-	newspaper_state_machine()
-	
-			
-
-func newspaper_state_machine():
-	
 	if Input.is_action_just_pressed('ui_right'):
-		next_paper()
+		pass
+		#next_paper()
 	if Input.is_action_just_pressed('ui_left'):
-		prev_paper()
+		pass
+		#prev_paper()
 	
-	set_arrows()
-	set_page()
-		
-	if paper_changed:
-		paper_changed = false
-		if active_paper < len(texts):
-			set_text(texts[active_paper])
-		else:
-			return true
-			#$Newspaper.visible = false
-			#$Newspaper.queue_free()
-	return false
+	
--- Final/Scripts/Reading.gd
+++ Final/Scripts/Reading.gd
@@ -3,11 +3,27 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var timer = null
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	_start_timer()
 	$CanvasLayer/BlackFader.fadeIn()
+	
+
+func _start_timer():
+	# ANY BIRD WORDS GO HERE
+	self.timer = Timer.new()
+	self.timer.wait_time = 6
+	add_child(self.timer)
+	self.timer.connect('timeout', self, '_end_timer')
+	self.timer.start()
+	$CanvasLayer/UI.visible = false
+	
+func _end_timer():
+	self.timer.stop()
+	$CanvasLayer/UI.visible = true
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
