54acd55e7235f835d76a11312411aeae06dd239c
Forest Anderson <wizcardforest@gmail.com>
2018-01-27 23:31:59 -0500

    Small changes to physics

--- Final/Scripts/Player.gd
+++ Final/Scripts/Player.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 var turn_velocity = 0
 var velocity_multiplier = 3
 var base_speed = 0.1
+var max_turn_speed = 0.08
+var player_velocity = Vector2(0, 0)
+
 export(float,0,10,0.5) var temp_speed = 5
 var max_box = {}
 
@@ -22,27 +25,38 @@ func _ready():
 	$Camera.limit_left = self.max_box.minx*3
 	$Camera.limit_top = self.max_box.miny*3
 	$Camera.limit_right = self.max_box.maxx*3
-	$Camera.limit_bottom = self.max_box.maxy*3	
+	$Camera.limit_bottom = self.max_box.maxy*3
 
 func _process(delta):
+	#print(turn_velocity)
 	if turn_velocity > 0:
-		turn_velocity -= min(0.01, turn_velocity)
+		turn_velocity -= min(0.4 * delta, turn_velocity)
 	elif turn_velocity < 0:
-		turn_velocity += min(0.01, turn_velocity * -1)
+		turn_velocity += min(0.4 * delta, turn_velocity * -1)
 		
 	if Input.is_action_pressed("ui_a"):
-		turn_velocity -= 0.015
+		turn_velocity -= 1.4 * delta
 	if Input.is_action_pressed("ui_d"):
-		turn_velocity += 0.015
+		turn_velocity += 1.4 * delta
 	
-	if turn_velocity > 0.05:
-		turn_velocity = 0.05
-	elif turn_velocity < -0.05:
-		turn_velocity = -0.05
+	if turn_velocity > max_turn_speed:
+		turn_velocity = max_turn_speed
+	elif turn_velocity < -max_turn_speed:
+		turn_velocity = -max_turn_speed
 		
-	velocity_multiplier = 1 - pow(0.05 - abs(turn_velocity), 2)
+	velocity_multiplier = 1 + (1 * (pow(abs(turn_velocity * 3), 1)))
 	
 	self.rotation += turn_velocity
-		
-	self.position.x += cos(self.rotation - 1.57) * temp_speed
-	self.position.y += sin(self.rotation - 1.57) * temp_speed
+	
+	player_velocity.x = (cos(self.rotation - 1.57)) * velocity_multiplier * delta * 250
+	player_velocity.y = (sin(self.rotation - 1.57)) * velocity_multiplier * delta * 250
+	
+	print(player_velocity.x)
+	
+	self.position.x += player_velocity.x
+	self.position.y += player_velocity.y
+	
+	#Simple border control
+	if (self.position.x < -2900 or self.position.x > 2900 or self.position.y < -1900 or self.position.y > 1900):
+		self.position.x -= (cos(self.rotation - 1.57) * 4) * velocity_multiplier * delta
+		self.position.y -= (sin(self.rotation - 1.57) * 4) * velocity_multiplier * delta
