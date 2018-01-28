fc7cd0872f08c5306a6f2021fc993883bf19b838
Christopher Blackman <ChristopherBlackman@users.noreply.github.com>
2018-01-28 01:46:39 -0500

    THE PACKAGE COMRAD

--- Final/Scripts/ThePackage.gd
+++ Final/Scripts/ThePackage.gd
@@ -4,6 +4,7 @@ extends Node2D
 # var a = 2
 # var b = "textvar"
 var splatter_class = preload('res://Scenes/Splatter.tscn')
+
 export(Vector2) var velocity = Vector2(0,0)
 export(float,0,1) var scalar = 0.8
 
@@ -11,18 +12,21 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	$Anime.play('Fall')
-	set_process(true)
-	#$Anime.connect('animation_finished',
+	#$Anime.
+	#splat()
+	set_physics_process(true)
+	#$Anime.connect('animation_finished',self,'splat')
 	pass
 
-func _process(delta):
+func _physics_process(delta):
+	if not $Anime.is_playing():
+		splat()
+		
 	self.position += velocity*delta*scalar
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+
 func splat():
 	var splatter = splatter_class.instance()
-	get_tree().root().add_child(splatter)
+	get_tree().root.add_child(splatter)
 	splatter.position = self.position
+	self.queue_free()
