0906063f26a96801a0d1ee59b6ecf1fe04b70a32
Connor Hillen <connor.hillen@gmail.com>
2018-01-27 22:13:53 -0500

    Updated camera to follow player and avoid crossing boundary

--- Final/Scripts/Player.gd
+++ Final/Scripts/Player.gd
@@ -4,11 +4,25 @@ var turn_velocity = 0
 var velocity_multiplier = 3
 var base_speed = 0.1
 export(float,0,10,0.5) var temp_speed = 5
+var max_box = {}
 
 func _ready():
 	self.position.x = 800
 	self.position.y = 800
 	self.add_to_group('player')
+	var topleft = get_parent().get_node('Mapmap').get_node('Topleft')
+	var bottomright = get_parent().get_node('Mapmap').get_node('Bottomright')
+	self.max_box = {
+		'minx': topleft.position.x,
+		'miny': topleft.position.y, 
+		'maxx': bottomright.position.x, 
+		'maxy': bottomright.position.y
+	}
+
+	$Camera.limit_left = self.max_box.minx*3
+	$Camera.limit_top = self.max_box.miny*3
+	$Camera.limit_right = self.max_box.maxx*3
+	$Camera.limit_bottom = self.max_box.maxy*3	
 
 func _process(delta):
 	if turn_velocity > 0:
